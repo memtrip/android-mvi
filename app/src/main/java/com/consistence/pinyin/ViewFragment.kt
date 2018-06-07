eb6d99991263a56e1cf466aad9f3a2677434a8b6
Samuel Kirton <samkirton@me.com>
2018-06-07 23:14:48 +0100

    * added coordinator layout for better scrolling on smaller devices

--- app/src/main/java/com/consistence/pinyin/ViewFragment.kt
+++ app/src/main/java/com/consistence/pinyin/ViewFragment.kt
@@ -1,7 +1,11 @@
 package com.consistence.pinyin
 
 import android.content.Context
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import io.reactivex.disposables.CompositeDisposable
 
 abstract class ViewFragment<I : ViewIntent, S : ViewState, M : Model<I, S>, R : ViewRender<S>> : Fragment() {
